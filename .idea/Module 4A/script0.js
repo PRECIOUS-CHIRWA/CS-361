77ee648b00b192622a898ca0b9cbf05ffb97fbaf
PRECIOUS-CHIRWA <preciousc74@outlook.com>
2026-07-20 17:53:39 +0200

    Module 4A script.js

--- .idea/Module 4A/script0.js
+++ .idea/Module 4A/script0.js
@@ -1,7 +1,10 @@
+//Task 1 Setup
 console.log("Precious Chirwa");
 
 alert("Welcome to Module 4: JavaScript Basics!");
 
+
+//Task 2 Variables and Types
 const studentName = "Precious Chirwa";
 const university = "The Copperbelt university";
 const program = "CS";
@@ -20,7 +23,8 @@ currentYear = 4;
 console.log("Updated Age: ${age}");
 console.log("Updated Year: ${currentYear}");
 
-//Simple calculator
+
+//Task 3 Simple calculator
 const num1 = 7;
 const num2 = 9;
 
@@ -31,12 +35,12 @@ console.log(`The division of ${num1} and ${num2} is ${num1 / num2}`);
 console.log(`The modulo of ${num1} and ${num2} is ${num1 % num2}`);
 console.log(`The power of ${num1} to the ${num2} is ${num1 ** num2}`);
 
-//Strings practice
+
+// Task 4 String methods practice
 let greet1 = "Hi, my name is " + studentName;
 const fullGreeting = `${greet1} I am a ${currentYear} student at ${university}.`;
 console.log(fullGreeting);
 
-// String methods
 //Length of name
 console.log(`Length of my name: ${studentName.length} characters`);
 
@@ -53,12 +57,44 @@ console.log(`Split my name into 2 words: ${studentName.split(" ")}`);
 console.log(`Replace Copperbelt with CBU: ${university.replace("The Copperbelt university", "CBU")}`);
 
 //includes
-console.log(`Does my name include \'re\'? ${studentName.includes('re')}`);
+console.log(`Does my name include 're'? ${studentName.includes('re')}`);
 
 //trim
 let hobbies = "  Reading,  Cooking  ";
 console.log(`Trimmed hobbies: ${hobbies.trim()}`);
 
-//Comparison logic
-let score = 99;
+
+//Task 5 Comparison logic
+let score = 96;
+
+console.log(`Is the score between 50 and 59?` + (score >= 50 && score <= 59) ? "Yes. Your score is grade is C" :
+    `Is your score between 60 and 69?` + (score >= 60 && score <= 69) ? "Yes. Your score is grade is B"
+        : `Is the score between 70 and 76? ` + (score >= 70 && score <= 76) ? "Yes. Your score is grade is B+"
+            : `Is your score between 77 and 85? ` + (score >= 77 && score <= 85) ? "Yes. Your score is grade is A"
+                : `Is your score between 86 and 100? ` + (score >= 86 && score <= 100) ? "Yes. Your score is grade is A+"
+                    : "Fail.");
+
+
+
+//Task 6 Grade decider
+let score0 = 97;
+
+if (score0 >= 50 && score0 <= 59) {
+    console.log("Pass");
+}
+else if (score0 >= 60 && score0 <= 69) {
+    console.log("Credit");
+}
+else if (score >= 70 && score <= 76) {
+    console.log("Merit 2");
+}
+else if (score >= 77 && score <= 85) {
+    console.log("Merit 1");
+}
+else if (score >= 86 && score <= 100) {
+    console.log("Distinction");
+}
+else {
+    console.log("Fail");
+}
 
